de2a50ce5eaa2bae01fbb6a09a0c4c4d1f88d260
Raymond Edeamrere <rayedeam@gmail.com>
2022-11-08 14:47:42 -0700

    testing SonarQube

--- JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,10 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		String DBPassword ="Fabre4gas"
+
+		System.out.println("Hello");
+		System.out.println("World");
 	}
 
 }
